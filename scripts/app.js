723a876d44cbb95f22c9b6bfeac9d88cb4c3e127
J2b2590 <J2b2590@gmail.com>
2021-04-09 15:36:27 -0500

    working on a scroll to function

--- scripts/app.js
+++ scripts/app.js
@@ -1,55 +1,22 @@
-console.log("connected")
+console.log("connected");
 
-//Color transistion 
-$(window).scroll(function() {
-// 100 = The point you would like to fade the nav in.
-  
-	if ($(window).scrollTop() > 50 ){
-    
- 		$('.bg').addClass('show');
-    
+//Color transistion
+$(window).scroll(function () {
+  // 100 = The point you would like to fade the nav in.
+
+  if ($(window).scrollTop() > 50) {
+    $(".bg").addClass("show");
   } else {
-    
-    $('.bg').removeClass('show');
-    
- 	};   	
+    $(".bg").removeClass("show");
+  }
 });
 
-
-// $(document).ready(function() {
-//     $(".dropdown-toggle").dropdown();
-// });
-
-
-
-
-// $('#btn-id').click(function(e){
-//     e.stopPropagation()
-// });
-
-// $("document").ready(function(){
-//   $("button").click(function(){
-// 	  if($("button").text() == "✖"){
-// 	  $("button").text("☰");
-// 	}else{        
-// 	  $("button").text("✖");      
-// 	}
-// 	$("li").toggle("slow");
-//     });  
-//   });
-
-
-// $(document).ready(function(){
-// 		    $('a[href^="#"]').on('click',function (e) {
-// 		        e.preventDefault();
-// 		        var target = this.hash;
-// 		        $target = $(target);
-// 		        console.log(this.hash, "haghs")
-// 		        $('html, body').stop().animate({
-// 		            'scrollTop':  $target.offset().top
-// 		        }, 1200, 'swing', function () {
-// 		            window.location.hash = target;
-// 		        });
-// 	    	});
-// 		});
-
+$("#projButton").click(function () {
+  console.log("CLICKED BUTTON");
+  //   $("html,body").animate(
+  //     {
+  //       scrollTop: $(".projSummary").offset().top,
+  //     },
+  //     "slow"
+  //   );
+});
